55f81d5fe78379129b787f0e4de4eb3bfebb03d9
eromanmat <eromanmat79@gmail.com>
2024-06-14 12:06:25 +0200

    task3

---
+++ task3.py
@@ -0,0 +1,107 @@
+
+
+"""
+Часть 3. Во всех заданиях данной группы требуется вывести логическое значение True,
+если приведенное высказывание для предложенных исходных данных является истинным, и значение False в противном случае. Все числа,
+для которых указано количество цифр (двузначное число, трехзначное число и т. д.), считаются целыми положительными.
+"""
+
+# Дано целое число A. Проверить истинность высказывания: «Число A является положительным».
+
+# number = int(input("Enter side: "))
+
+# if number > 0:
+#     print(True)
+# else:
+#     print(False)
+
+# Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».
+
+# number = int(input("Enter side: "))
+
+# if number % 2 == 1:
+#     print(True)
+# else:
+#     print(False)
+
+# Дано целое число A. Проверить истинность высказывания: «Число A является четным».
+
+# number = int(input("Enter side: "))
+
+# if number % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны два целых числа: A, B. Проверить истинность высказывания: «Справедливы неравенства A > 2 и B ≤ 3».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+
+# if a > 2 and b <= 3:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны два целых числа: A, B. Проверить истинность высказывания: «Справедливы неравенства A ≥ 0 или B < −2».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+
+# if a >= 0 and b < -2:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < _C_».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+# c = int(input("Enter side: "))
+
+# if a < b < c:
+#     print(True)
+# else:
+#     print(False)
+
+
+# Даны три целых числа: A, B, C. Проверить истинность высказывания: «Число B находится между числами A и _C_».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+# c = int(input("Enter side: "))
+
+# if a < b < c:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны два целых числа: A, B. Проверить истинность высказывания: «Каждое из чисел A и B нечетное».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+
+# if a % 2 == 1 and b % 2 ==1:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны два целых числа: A, B. Проверить истинность высказывания: «Хотя бы одно из чисел A и B нечетное».
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+
+# if a % 2 == 1 or b % 2 ==1:
+#     print(True)
+# else:
+#     print(False)
+
+# Даны два целых числа: A, B. Проверить истинность высказывания: «Ровно одно из чисел A и B нечетное»
+
+# a = int(input("Enter side: "))
+# b = int(input("Enter side: "))
+
+# if (a % 2 == 1 and b % 2 == 0) or (a % 2 == 0 and b % 2 == 1):
+#     print(True)
+# else:
+#     print(False)
