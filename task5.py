301c77c3dbd916782e34ea368ba58da67a7e80f6
eromanmat <eromanmat79@gmail.com>
2024-06-15 19:20:56 +0200

    task5

---
+++ task5.py
@@ -0,0 +1,65 @@
+
+
+
+# Часть 5. Словари
+
+
+# Создайте словарь содержащий данные о человеке. В качестве строковых ключей используйте его имя, возраст, пол, рост, вес, размер ноги.
+
+# user_data = {}
+
+# user_data['name'] = input("Введите имя: ")
+# user_data['age'] = int(input("Введите возраст: "))
+# user_data['gender'] = input("Введите пол: ")
+# user_data['height'] = int(input("Введите рост (в см): "))
+# user_data['weight'] = float(input("Введите вес (в кг): "))
+
+# print(user_data)
+
+# Выведите на экран все данные о человеке по ключам.
+
+# user_data = {}
+
+# user_data['name'] = input("Введите имя: ")
+# user_data['age'] = int(input("Введите возраст: "))
+# user_data['gender'] = input("Введите пол: ")
+# user_data['height'] = int(input("Введите рост (в см): "))
+# user_data['weight'] = float(input("Введите вес (в кг): "))
+
+# for key in user_data:
+#     print(f"{key}: {user_data[key]}")
+
+# Добавьте в словарь ключ и значение размера ноги
+
+# user_data = {
+#     'name': 'Arur',
+#     'age': 25,
+#     'gender': 'men',
+#     'height': 180,
+#     'weight': 75.5
+# }
+
+# print(user_data)
+
+# user_data.update({
+#     'shoe size': 43
+# })
+
+# print(user_data)
+
+# Удалите из словаря возраст.
+
+# user_data = {
+#     'name': 'Arur',
+#     'age': 25,
+#     'gender': 'men', 
+#     'height': 180, 
+#     'weight': 75.5, 
+#     'shoe size': 43
+# }
+
+# del user_data['age']
+
+# for key in user_data:
+#     print(f"{key}: {user_data[key]}")
+
